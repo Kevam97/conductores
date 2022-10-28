1bf273e10a798b8208c9d654a4c80cbc48206738
Kevin <k.valencia2@utp.edu.co>
2022-10-28 02:06:27 -0500

    cambios vista offers

--- resources/views/livewire/offer-view.blade.php
+++ resources/views/livewire/offer-view.blade.php
@@ -23,6 +23,7 @@
                                     </a>
                                 </div>
                             </div>
+                            <br>
                         @endforeach
                         {{ $drivers->links() }}
                     </div>
@@ -55,7 +56,6 @@
                                                 alt="Wild Landscape"/>
                                             </div>
                                         @endforeach
-                                        {{ $drivers->links() }}
                                     </div>
                                     <button
                                       class="carousel-control-prev absolute top-0 bottom-0  bg-yellow-400  flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
@@ -88,6 +88,7 @@
                         @endif
                         </form>
                         @endforeach
+                        {{ $owners->links() }}
                     </div>
                 </div>
             </div>
