e33aea00936ee84382f6da896e42b780b706dd82
Kevin <k.valencia2@utp.edu.co>
2022-10-28 01:58:53 -0500

    paginacion offers

--- resources/views/livewire/offer-view.blade.php
+++ resources/views/livewire/offer-view.blade.php
@@ -24,6 +24,7 @@
                                 </div>
                             </div>
                         @endforeach
+                        {{ $drivers->links() }}
                     </div>
                     <div class="flex justify-center flex-col px-6 rounded-lg shadow-lg bg-neutral-900 max-w ">
                         <h2 class="font-semibold text-xl text-white  text-center leading-tight">
@@ -54,6 +55,7 @@
                                                 alt="Wild Landscape"/>
                                             </div>
                                         @endforeach
+                                        {{ $drivers->links() }}
                                     </div>
                                     <button
                                       class="carousel-control-prev absolute top-0 bottom-0  bg-yellow-400  flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
